3a517d1505850f71b97c582d54885e5199a60271
Egor Vasyakin <evasyakin@mail.ru>
2020-02-29 06:40:11 +0700

    Create HttpException.php

---
+++ src/HttpException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package evas-php/evas-http
+ */
+namespace Evas\Http;
+
+use \Exception;
+
+/**
+ * Класс исключения модуля http.
+ * @author Egor Vasyakin <egor@evas-php.com>
+ * @since 1.0
+ */
+class HttpException extends Exception
+{}

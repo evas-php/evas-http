18e0c0845faaa02e908c486b42d98c2a862855a7
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-05-06 03:22:28 +0300

    Create ResponseInterface.php

---
+++ src/Interfaces/ResponseInterface.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Интерфейс ответа.
+ * @package evas-php\evas-http
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Http\Interfaces;
+
+interface ResponseInterface
+{
+    /**
+     * Установка кода статуса.
+     * @param int
+     * @throws Exception
+     * @return self
+     */
+    public function withStatusCode(int $code);
+
+    /**
+     * Установка тела.
+     * @param string
+     * @return self
+     */
+    public function withBody(string $body);
+
+    /**
+     * Установка тела с преобразованием в json.
+     * @param mixed
+     * @return self
+     */
+    public function withBodyJson($body);
+
+
+
+    /**
+     * Получение кода статуса.
+     * @return int
+     */
+    public function getStatusCode(): int;
+
+    /**
+     * Получение тела.
+     * @return string
+     */
+    public function getBody(): string;
+
+
+
+    /**
+     * Отправка.
+     * @param int|null код статуса
+     * @param string|null тело
+     * @param array|null заголовки
+     */
+    public function send(int $code = null, string $body = null, array $headers = null);
+
+    /**
+     * Отправка с преобразованием тела в json.
+     * @param int|null код статуса
+     * @param mixed|null тело
+     * @param array|null заголовки
+     */
+    public function sendJson(int $code = null, $body = null, array $headers = null);
+}
